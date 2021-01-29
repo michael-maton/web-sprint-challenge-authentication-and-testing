87ca6275ea640b0f50c14bb72f2ebde03bc447af
Michael Maton <mjmaton6@gmail.com>
2021-01-28 20:27:57 -0600

    2 tests for GET/api/jokes written

--- api/server.test.js
+++ api/server.test.js
@@ -6,7 +6,6 @@ test("sanity", () => {
 const request = require("supertest");
 const server = require("./server");
 const db = require("./../data/dbConfig");
-const Users = require("./users/users-model");
 
 beforeAll(async () => {
   await db.migrate.rollback();
@@ -19,8 +18,8 @@ afterAll(async () => {
   await db.destroy();
 });
 
-const user1 = { username: "bffob", password: "1234" };
-const user2 = { username: "mdsfe12", password: "1234" };
+const user1 = { username: "user1", password: "1234" };
+const user2 = { username: "user2", password: "1234" };
 
 describe("endpoints", () => {
   describe("[POST] /api/auth/register", () => {
@@ -51,4 +50,20 @@ describe("endpoints", () => {
     });
   });
 
+  describe("[GET] /api/jokes", () => {
+    it("responds with token required if not logged in", async () => {
+      const res = await (await request(server).get("/api/jokes"));
+      expect(res.body.message).toBe("token required");
+    });
+    it("responds with code 200 upon success", async () => {
+      //register and log user in
+      await request(server).post("/api/auth/register").send(user1);
+      const res = await request(server).post("/api/auth/login").send(user1);
+      //grab token
+      const token = res.body.token;
+      //GET request with token
+      const authRes = await request(server).get("/api/jokes").set({Authorization: token})
+      expect(authRes.status).toBe(200);
+    });
+  });
 });
