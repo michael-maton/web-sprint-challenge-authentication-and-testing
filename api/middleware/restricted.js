fd6f2b10433fc4e0c8eb8cf75809d82d16e26ba4
Michael Maton <mjmaton6@gmail.com>
2021-01-28 20:01:32 -0600

    restricted auth made for GET /api/jokes, passing all tests

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const { jwtSecret } = require("../../config/secrets.js");
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+
+  if(!token) {
+    res.status(401).json({ message: 'token required' })
+  } else {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if(err) {
+        res.status(401).json({ message: 'token invalid'})
+      } else {
+        req.decodedToken = decoded;
+        next();
+      }
+    })
+  }
   /*
     IMPLEMENT
 
